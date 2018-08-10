4a7ce416c2238365d6f45c284e10c358ca054218
Adel Moreno <adel.argenix@gmail.com>
2018-08-10 11:43:37 -0600

    added function to highlight words and find and replace with regular expressions

--- mutilator.js
+++ mutilator.js
@@ -1,5 +1,12 @@
+function highlightAll() {
+ 	var string = document.getElementById("words").innerHTML;
+ 	var res = string.replace(/BORK/gi, "<span class='highlight'>bork</span>");
+ 	document.getElementById("words").innerHTML = res;
+}
+
 function replaceAll() {
-	var string = document.getElementById("words").innerHTML;
-	var res = string.replace(/bork/gi, "BORK");
-	document.getElementById("words").innerHTML = res;
+	var r = /(\w+) (\w+)/gi;
+	var string = document.getElementById("dog").innerHTML;
+	var res = string.replace(r, "BORK");
+	document.getElementById("dog").innerHTML = res;
 }
