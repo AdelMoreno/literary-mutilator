048e1d93cfcc8aadcd8ec9205e0c6915874ad678
Adel Moreno <adel.argenix@gmail.com>
2018-08-13 19:15:07 -0600

    added functions to change text color, size, and font family

--- mutilator.js
+++ mutilator.js
@@ -1,12 +1,21 @@
 function highlightAll() {
  	var string = document.getElementById("words").innerHTML;
- 	var res = string.replace(/BORK/gi, "<span class='highlight'>BORK</span>");
- 	document.getElementById("words").innerHTML = res;
+ 	var result = string.replace(/BORK/gi, "<span class='highlight'>BORK</span>");
+ 	document.getElementById("words").innerHTML = result;
 }
 
 function replaceAll() {
 	var r = /\w{5,}\b/gi;
 	var string = document.getElementById("dog").innerHTML;
-	var res = string.replace(r, "BORK");
-	document.getElementById("dog").innerHTML = res;
+	var result = string.replace(r, "BORK");
+	document.getElementById("dog").innerHTML = result;
+}
+
+function color() {
+	document.getElementById("words").style.color ="red";
+}
+
+function font() {
+	document.getElementById("words").style.fontFamily="Brush Script MT";
+	document.getElementById("words").style.fontSize="150%"
 }
